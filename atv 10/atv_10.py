4e3180847cab7a0ead48e43df5cb3e4582800fd8
fabr404 <fabriciofelixsilva11@gmail.com>
2024-09-11 13:48:15 -0300

    arv10

---
+++ atv 10/atv_10.py
@@ -0,0 +1,10 @@
+# Crie um programa que receba um número inteiro e informe se ele é par ou ímpar.
+numero = int(input("digite um numero : "))
+
+if numero % 2 == 0:
+
+    print(f"o numero {numero} e par .")
+
+else:
+
+    print(f"o numero {numero} e inpar .")
